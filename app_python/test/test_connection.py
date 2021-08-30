708d91ec89a03b839dffaa4dd034cc2b2afce53f
Bekzhan Talgat <ezpzbaby@Bekzhans-MacBook-Pro.local>
2021-08-30 17:24:17 +0300

    Client for test

--- app_python/test/test_connection.py
+++ app_python/test/test_connection.py
@@ -1,6 +1,19 @@
 import pytest
 import requests
+import time
+from flask.testing import FlaskClient
+from app import app
+
+app.testing = True
+client = app.test_client()
 
 def test_app():
-    assert requests.get('http://127.0.0.1:5000/').status_code == 200
+    start = time.time()
+
+    assert client.get('http://127.0.0.1:5000/').status_code == 200
+
+    end = time.time()
+    delay = end - start
+
+    assert delay <= 1
 
