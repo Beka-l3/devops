620f474467abda23b197440a674acc7e62e00711
Beka-l3 <b.talgat@innopolis.university>
2021-10-04 23:55:15 +0300

    lab 13

--- app_python/app.py
+++ app_python/app.py
@@ -1,5 +1,7 @@
 """Simple python flask application"""
 from flask import Flask, render_template
+import pytz
+import datetime
 
 app = Flask(__name__)
 
@@ -9,7 +11,35 @@ def home_page():
     """
     Returns rendered index.htl as a home page
     """
+
+    timezone = pytz.timezone("Europe/Moscow")
+    server_time = datetime.datetime.now(timezone)
+    # print(str(server_time))
+    visits = []
+    with open("visits.txt", "r", encoding="utf-8") as file:
+        visits = file.readlines()
+    visits = visits[:100]
+    visits.insert(0, str(server_time)+'\n')
+    with open("visits.txt", "w", encoding="utf-8") as file:
+        file.writelines(visits)
+    
     return render_template("index.html")
+
+
+@app.route("/visits")
+def visits_page():
+    """
+    Returns number of times root path was accessed
+    Returns:
+        int: number of times was accessed
+    """
+    visits = []
+    with open("visits.txt", "r", encoding="utf-8") as file:
+        visits = file.readlines()
+    html_response = render_template(
+        "visits.html", visits=visits
+    )
+    return html_response
 
 
 if __name__ == "__main__":
